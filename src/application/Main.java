db4e11041f5cad725e268a24f05274999e96be4f
kostarasx <33222110+kostarasx@users.noreply.github.com>
2018-08-14 03:58:51 +0300

    Welcome Menu
    Adding the welcome menu image and create window

--- src/application/Main.java
+++ src/application/Main.java
@@ -11,9 +11,10 @@ public class Main extends Application {
 	public void start(Stage primaryStage) {
 		try {
 			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
+			Scene scene = new Scene(root,800,800);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
+			primaryStage.setResizable(true);
 			primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
