1b6f1df35e9b38659e9bbbb41ff4d48aa76c4e44
kostarasx <33222110+kostarasx@users.noreply.github.com>
2018-08-14 17:52:03 +0300

    Adding soundtrack
    Add soundtrack in welocme menu and minnor changes

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,12 +1,21 @@
 package application;
-	
+
+import java.nio.file.Paths;
+
 import javafx.application.Application;
+import javafx.geometry.Insets;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
 
 
 public class Main extends Application {
+		
+	MediaPlayer mediaPlayer;
 	
 	public static void main(String[] args) {
 		launch(args);
@@ -16,25 +25,44 @@ public class Main extends Application {
 	public void start(Stage primaryStage) {
 		try {
 			//Welcome Menu 
+			
 			//Creating the layout
 			BorderPane root = new BorderPane();
 			Scene scene = new Scene(root,800,800);
+
+			//Soundtrack
+			String bip = "Music/welcomemenu.mp3";
+			Media hit = new Media(Paths.get(bip).toUri().toString());
+			mediaPlayer = new MediaPlayer(hit);
+			mediaPlayer.setCycleCount(MediaPlayer.INDEFINITE);
+			mediaPlayer.setVolume(0.07);
+			mediaPlayer.play();
 			
-			//add background photo
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			
-			//Press any key to continue feature go to M
+			//Press any key to continue feature go to Main Menu
+			StackPane stack = new StackPane();
+			Label  startingText = new Label("PRESS ANY BUTTON TO CONTINUE");
+			root.setCenter(stack);
+			BorderPane.setMargin(stack, new Insets(120,80,80,120));
+			stack.getChildren().add(startingText);
+
 			scene.setOnKeyPressed(e -> System.out.println("Test"));
 			scene.setOnMousePressed(e ->System.out.println("Test" ));
+
+			//add custom styles 
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			
-			//Show the scene 
+			//Set and showing the scene
+			primaryStage.setTitle("Dark Souls Tools");
 			primaryStage.setScene(scene);
 			primaryStage.setResizable(false);//Cant resized
 			primaryStage.show();
-			
-			
+
+
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
 	}
 }
+
+
